f1410564dceb7c6b9bec91fe00a5080d9bad55eb
Akash1040-nm <nr8821314@gmail.com>
2024-11-17 19:40:59 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,7 +18,7 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : { file_name }
 
 sɪᴢᴇ ⚙️: {file_size}
 
